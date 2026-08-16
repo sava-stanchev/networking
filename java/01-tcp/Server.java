12ae6aaaf9dbfdd17590e09a279386b1dcdd166b
sava-stanchev <s.stanchev94@gmail.com>
2026-08-16 19:53:57 +0300

    basic tcp server

---
+++ java/01-tcp/Server.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	public static void main(String[] args) throws IOException {
+		ServerSocket server = new ServerSocket(8080);
+		System.out.println("listening on port 8080");
+		Socket client = server.accept();
+		System.out.println("client connected");
+		InputStream input = client.getInputStream();
+		int data = input.read();
+		System.out.println("received: " + (char) data);
+		client.close();
+		server.close();
+	}
+}
