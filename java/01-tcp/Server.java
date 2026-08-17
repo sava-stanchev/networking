64c745877e9c8ed96ba96ec6e6c8e665993a4dd9
sava-stanchev <s.stanchev94@gmail.com>
2026-08-17 23:15:26 +0300

    newline delimited tcp msgs

--- java/01-tcp/Server.java
+++ java/01-tcp/Server.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,11 +16,11 @@ public class Server {
 			
 			Thread thread = new Thread(() -> {
 				try {
-					InputStream input = client.getInputStream();
-					int data;
-					
-					while ((data = input.read()) != -1) {
-						System.out.print((char) data);
+					BufferedReader reader = new BufferedReader(
+						new InputStreamReader(client.getInputStream()));
+					String msg;
+					while ((msg = reader.readLine()) != null) {
+						System.out.println(client.getRemoteSocketAddress() + ": " + msg);
 					}
 					
 					System.out.println("\nclient disconnected: " + client.getRemoteSocketAddress());
